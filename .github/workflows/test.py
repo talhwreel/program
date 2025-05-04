58dbeeb4217555c15e0d59cd7b466082460ff6d0
talhwreel <anneziplatan3113@gmail.com>
2025-05-04 17:12:15 +0300

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -7,7 +7,7 @@ from pypresence import Presence
 
 LICENSES = {
     "kaancalismayan31": {"username": "Kaan", "expiration_date": "2025-12-31"},
-    "testkey": {"username": "Berkay Çalışkan", "expiration_date": "2025-05-05"},
+    "testkey": {"username": "Berkay Çalışkan", "expiration_date": "2025-05-06"},
 }
 
 def colored_text(text, color):
